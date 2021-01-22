aafcfacc246cabcae7cf480aa0c3ac4a176a1f59
user <avivaviv283@gmail.com>
2021-01-22 14:10:33 +0200

    ACHIEVED NOTHING

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -10,14 +10,19 @@ import android.widget.TextView;
 
 public class Game extends AppCompatActivity implements View.OnClickListener {
 
-    Button[][]btnsg1,btnsg2,btnsg3,btnsg4,btnsg5,btnsg6,btnsg7,btnsg8,btnsg9;
+    Button[]btnsg1,btnsg2,btnsg3,btnsg4,btnsg5,btnsg6,btnsg7,btnsg8,btnsg9;
     Button[] allBtns;
+
     Button btnResetGame;
+    Button[][] arr = {btnsg1, btnsg2, btnsg3,btnsg4,btnsg5,btnsg6,btnsg7,btnsg8,btnsg9};;
+    int goTo=0;
     Button btn;
     TextView tvWIn;
     Intent goMenu;
     int counter =0;
     int btnCount=0;
+    private Object Button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,184 +32,129 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         btnResetGame = (Button) findViewById(R.id.btnResetGame);
         btnResetGame.setOnClickListener(this);
         btnResetGame.setVisibility(View.INVISIBLE);
-        btnsg1 = new Button[3][3];
-        btnsg2 = new Button[3][3];
-        btnsg3 = new Button[3][3];
-        btnsg4 = new Button[3][3];
-        btnsg5 = new Button[3][3];
-        btnsg6 = new Button[3][3];
-        btnsg7 = new Button[3][3];
-        btnsg8 = new Button[3][3];
-        btnsg9 = new Button[3][3];
-        btnsg1[0][0] = (Button) findViewById(R.id.g11);
-        btnsg1[0][1] = (Button) findViewById(R.id.g12);
-        btnsg1[0][2] = (Button) findViewById(R.id.g13);
-        btnsg1[1][0] = (Button) findViewById(R.id.g14);
-        btnsg1[1][1] = (Button) findViewById(R.id.g15);
-        btnsg1[1][2] = (Button) findViewById(R.id.g16);
-        btnsg1[2][0] = (Button) findViewById(R.id.g17);
-        btnsg1[2][1] = (Button) findViewById(R.id.g18);
-        btnsg1[2][2] = (Button) findViewById(R.id.g19);
+        btnsg1 = new Button[9];
+        btnsg2 = new Button[9];
+        btnsg3 = new Button[9];
+        btnsg4 = new Button[9];
+        btnsg5 = new Button[9];
+        btnsg6 = new Button[9];
+        btnsg7 = new Button[9];
+        btnsg8 = new Button[9];
+        btnsg9 = new Button[9];
+        btnsg1[0] = (Button) findViewById(R.id.g11);
+        btnsg1[1] = (Button) findViewById(R.id.g12);
+        btnsg1[2] = (Button) findViewById(R.id.g13);
+        btnsg1[3] = (Button) findViewById(R.id.g14);
+        btnsg1[4] = (Button) findViewById(R.id.g15);
+        btnsg1[5] = (Button) findViewById(R.id.g16);
+        btnsg1[6] = (Button) findViewById(R.id.g17);
+        btnsg1[7] = (Button) findViewById(R.id.g18);
+        btnsg1[8] = (Button) findViewById(R.id.g19);
 
 
-        btnsg2[0][0] = (Button) findViewById(R.id.g21);
-        btnsg2[0][1] = (Button) findViewById(R.id.g22);
-        btnsg2[0][2] = (Button) findViewById(R.id.g23);
-        btnsg2[1][0] = (Button) findViewById(R.id.g24);
-        btnsg2[1][1] = (Button) findViewById(R.id.g25);
-        btnsg2[1][2] = (Button) findViewById(R.id.g26);
-        btnsg2[2][0] = (Button) findViewById(R.id.g27);
-        btnsg2[2][1] = (Button) findViewById(R.id.g28);
-        btnsg2[2][2] = (Button) findViewById(R.id.g29);
+        btnsg2[0] = (Button) findViewById(R.id.g11);
+        btnsg2[1] = (Button) findViewById(R.id.g12);
+        btnsg2[2] = (Button) findViewById(R.id.g13);
+        btnsg2[3] = (Button) findViewById(R.id.g14);
+        btnsg2[4] = (Button) findViewById(R.id.g15);
+        btnsg2[5] = (Button) findViewById(R.id.g16);
+        btnsg2[6] = (Button) findViewById(R.id.g17);
+        btnsg2[7] = (Button) findViewById(R.id.g18);
+        btnsg2[8] = (Button) findViewById(R.id.g19);
 
-        btnsg3[0][0] = (Button) findViewById(R.id.g31);
-        btnsg3[0][1] = (Button) findViewById(R.id.g32);
-        btnsg3[0][2] = (Button) findViewById(R.id.g33);
-        btnsg3[1][0] = (Button) findViewById(R.id.g34);
-        btnsg3[1][1] = (Button) findViewById(R.id.g35);
-        btnsg3[1][2] = (Button) findViewById(R.id.g36);
-        btnsg3[2][0] = (Button) findViewById(R.id.g37);
-        btnsg3[2][1] = (Button) findViewById(R.id.g38);
-        btnsg3[2][2] = (Button) findViewById(R.id.g39);
+        btnsg3[0] = (Button) findViewById(R.id.g11);
+        btnsg3[1] = (Button) findViewById(R.id.g12);
+        btnsg3[2] = (Button) findViewById(R.id.g13);
+        btnsg3[3] = (Button) findViewById(R.id.g14);
+        btnsg3[4] = (Button) findViewById(R.id.g15);
+        btnsg3[5] = (Button) findViewById(R.id.g16);
+        btnsg3[6] = (Button) findViewById(R.id.g17);
+        btnsg3[7] = (Button) findViewById(R.id.g18);
+        btnsg3[8] = (Button) findViewById(R.id.g19);
 
-        btnsg4[0][0] = (Button) findViewById(R.id.g41);
-        btnsg4[0][1] = (Button) findViewById(R.id.g42);
-        btnsg4[0][2] = (Button) findViewById(R.id.g43);
-        btnsg4[1][0] = (Button) findViewById(R.id.g44);
-        btnsg4[1][1] = (Button) findViewById(R.id.g45);
-        btnsg4[1][2] = (Button) findViewById(R.id.g46);
-        btnsg4[2][0] = (Button) findViewById(R.id.g47);
-        btnsg4[2][1] = (Button) findViewById(R.id.g48);
-        btnsg4[2][2] = (Button) findViewById(R.id.g49);
+        btnsg4[0] = (Button) findViewById(R.id.g11);
+        btnsg4[1] = (Button) findViewById(R.id.g12);
+        btnsg4[2] = (Button) findViewById(R.id.g13);
+        btnsg4[3] = (Button) findViewById(R.id.g14);
+        btnsg4[4] = (Button) findViewById(R.id.g15);
+        btnsg4[5] = (Button) findViewById(R.id.g16);
+        btnsg4[6] = (Button) findViewById(R.id.g17);
+        btnsg4[7] = (Button) findViewById(R.id.g18);
+        btnsg4[8] = (Button) findViewById(R.id.g19);
 
-        btnsg5[0][0] = (Button) findViewById(R.id.g51);
-        btnsg5[0][1] = (Button) findViewById(R.id.g52);
-        btnsg5[0][2] = (Button) findViewById(R.id.g53);
-        btnsg5[1][0] = (Button) findViewById(R.id.g54);
-        btnsg5[1][1] = (Button) findViewById(R.id.g55);
-        btnsg5[1][2] = (Button) findViewById(R.id.g56);
-        btnsg5[2][0] = (Button) findViewById(R.id.g57);
-        btnsg5[2][1] = (Button) findViewById(R.id.g58);
-        btnsg5[2][2] = (Button) findViewById(R.id.g59);
+        btnsg5[0] = (Button) findViewById(R.id.g11);
+        btnsg5[1] = (Button) findViewById(R.id.g12);
+        btnsg5[2] = (Button) findViewById(R.id.g13);
+        btnsg5[3] = (Button) findViewById(R.id.g14);
+        btnsg5[4] = (Button) findViewById(R.id.g15);
+        btnsg5[5] = (Button) findViewById(R.id.g16);
+        btnsg5[6] = (Button) findViewById(R.id.g17);
+        btnsg5[7] = (Button) findViewById(R.id.g18);
+        btnsg5[8] = (Button) findViewById(R.id.g19);
 
-        btnsg6[0][0] = (Button) findViewById(R.id.g61);
-        btnsg6[0][1] = (Button) findViewById(R.id.g62);
-        btnsg6[0][2] = (Button) findViewById(R.id.g63);
-        btnsg6[1][0] = (Button) findViewById(R.id.g64);
-        btnsg6[1][1] = (Button) findViewById(R.id.g65);
-        btnsg6[1][2] = (Button) findViewById(R.id.g66);
-        btnsg6[2][0] = (Button) findViewById(R.id.g67);
-        btnsg6[2][1] = (Button) findViewById(R.id.g68);
-        btnsg6[2][2] = (Button) findViewById(R.id.g69);
+        btnsg6[0] = (Button) findViewById(R.id.g11);
+        btnsg6[1] = (Button) findViewById(R.id.g12);
+        btnsg6[2] = (Button) findViewById(R.id.g13);
+        btnsg6[3] = (Button) findViewById(R.id.g14);
+        btnsg6[4] = (Button) findViewById(R.id.g15);
+        btnsg6[5] = (Button) findViewById(R.id.g16);
+        btnsg6[6] = (Button) findViewById(R.id.g17);
+        btnsg6[7] = (Button) findViewById(R.id.g18);
+        btnsg6[8] = (Button) findViewById(R.id.g19);
 
-        btnsg7[0][0] = (Button) findViewById(R.id.g71);
-        btnsg7[0][1] = (Button) findViewById(R.id.g72);
-        btnsg7[0][2] = (Button) findViewById(R.id.g73);
-        btnsg7[1][0] = (Button) findViewById(R.id.g74);
-        btnsg7[1][1] = (Button) findViewById(R.id.g75);
-        btnsg7[1][2] = (Button) findViewById(R.id.g76);
-        btnsg7[2][0] = (Button) findViewById(R.id.g77);
-        btnsg7[2][1] = (Button) findViewById(R.id.g78);
-        btnsg7[2][2] = (Button) findViewById(R.id.g79);
+        btnsg7[0] = (Button) findViewById(R.id.g11);
+        btnsg7[1] = (Button) findViewById(R.id.g12);
+        btnsg7[2] = (Button) findViewById(R.id.g13);
+        btnsg7[3] = (Button) findViewById(R.id.g14);
+        btnsg7[4] = (Button) findViewById(R.id.g15);
+        btnsg7[5] = (Button) findViewById(R.id.g16);
+        btnsg7[6] = (Button) findViewById(R.id.g17);
+        btnsg7[7] = (Button) findViewById(R.id.g18);
+        btnsg7[8] = (Button) findViewById(R.id.g19);
 
-        btnsg8[0][0] = (Button) findViewById(R.id.g81);
-        btnsg8[0][1] = (Button) findViewById(R.id.g82);
-        btnsg8[0][2] = (Button) findViewById(R.id.g83);
-        btnsg8[1][0] = (Button) findViewById(R.id.g84);
-        btnsg8[1][1] = (Button) findViewById(R.id.g85);
-        btnsg8[1][2] = (Button) findViewById(R.id.g86);
-        btnsg8[2][0] = (Button) findViewById(R.id.g87);
-        btnsg8[2][1] = (Button) findViewById(R.id.g88);
-        btnsg8[2][2] = (Button) findViewById(R.id.g89);
+        btnsg8[0] = (Button) findViewById(R.id.g11);
+        btnsg8[1] = (Button) findViewById(R.id.g12);
+        btnsg8[2] = (Button) findViewById(R.id.g13);
+        btnsg8[3] = (Button) findViewById(R.id.g14);
+        btnsg8[4] = (Button) findViewById(R.id.g15);
+        btnsg8[5] = (Button) findViewById(R.id.g16);
+        btnsg8[6] = (Button) findViewById(R.id.g17);
+        btnsg8[7] = (Button) findViewById(R.id.g18);
+        btnsg8[8] = (Button) findViewById(R.id.g19);
 
-        btnsg9[0][0] = (Button) findViewById(R.id.g91);
-        btnsg9[0][1] = (Button) findViewById(R.id.g92);
-        btnsg9[0][2] = (Button) findViewById(R.id.g93);
-        btnsg9[1][0] = (Button) findViewById(R.id.g94);
-        btnsg9[1][1] = (Button) findViewById(R.id.g95);
-        btnsg9[1][2] = (Button) findViewById(R.id.g96);
-        btnsg9[2][0] = (Button) findViewById(R.id.g97);
-        btnsg9[2][1] = (Button) findViewById(R.id.g98);
-        btnsg9[2][2] = (Button) findViewById(R.id.g99);
+        btnsg9[0] = (Button) findViewById(R.id.g11);
+        btnsg9[1] = (Button) findViewById(R.id.g12);
+        btnsg9[2] = (Button) findViewById(R.id.g13);
+        btnsg9[3] = (Button) findViewById(R.id.g14);
+        btnsg9[4] = (Button) findViewById(R.id.g15);
+        btnsg9[5] = (Button) findViewById(R.id.g16);
+        btnsg9[6] = (Button) findViewById(R.id.g17);
+        btnsg9[7] = (Button) findViewById(R.id.g18);
+        btnsg9[8] = (Button) findViewById(R.id.g19);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg1[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg1[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg2[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg2[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg3[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg3[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg4[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg4[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg5[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg5[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg6[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg6[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg7[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg7[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                btnsg8[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg8[i][j];
-                btnCount++;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++){
-                btnsg9[i][j].setOnClickListener(this);
-                allBtns[btnCount] = btnsg9[i][j];
-                btnCount++;
-            }
-        }
-
-
-
+        setArr(btnsg1);
+        setArr(btnsg2);
+        setArr(btnsg3);
+        setArr(btnsg4);
+        setArr(btnsg5);
+        setArr(btnsg6);
+        setArr(btnsg7);
+        setArr(btnsg8);
+        setArr(btnsg9);
 
 
     }
 
+    public void setArr(Button[] btnsg)
+    {
+
+            for (int j = 0; j < 9; j++)
+            {
+                btnsg[j].setOnClickListener(this);
+                allBtns[btnCount] = btnsg[j];
+                btnCount++;
+            }
+        }
 
 
 
@@ -262,55 +212,42 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
                 btnResetGame.setVisibility(View.VISIBLE);
             }
         }
+
+        Button[] temp = new Button[9];
+        for (int i = 0; i < allBtns.length; i++) {
+            if (allBtns[i] == v) {
+                goTo = (i + 1) % 9;
+    }
+
+    public void disable(Button[] arr){
+
+
+
+
+
+
+
     }
 
 
+    public int getGoTo(View v) {
 
-    public int isWin()
-    {
-        if(counter > 4)
-        {
-            int j = 0;
-            for (int i = 0; i < 3; i++) {
-                if (btns[i][j].getText().toString().length() > 0 && btns[i][j].getText().toString().equals(btns[i][j + 1].getText().toString()) && btns[i][j].getText().toString().equals(btns[i][j + 2].getText().toString())) {
-                    if (btns[i][j].getText().toString().equals("O"))
-                        return 1;//0 win  - odd counting
-                    return 2;//x win
-                }
-                else if (btns[j][i].getText().toString().length() > 0 && btns[j][i].getText().toString().equals(btns[j + 1][i].getText().toString()) && btns[j][i].getText().toString().equals(btns[j + 2][i].getText().toString())) {
-                    if (btns[j][i].getText().toString().equals("O"))
-                        return 1;//user win - even win
-                    return 2;
-                }
-
-            }//end of for
-            //checking diagonal
-            if (btns[0][0].getText().toString().length() > 0 && btns[0][0].getText().toString().equals(btns[1][1].getText().toString()) && btns[0][0].getText().toString().equals(btns[2][2].getText().toString())) {
-                if (btns[0][j].getText().toString().equals("O"))
-                    return 1;//0 win  - odd counting
-                return 2;//x win
-            }
-            if (btns[0][2].getText().toString().length() > 0 && btns[0][2].getText().toString().equals(btns[1][1].getText().toString()) && btns[0][2].getText().toString().equals(btns[2][0].getText().toString())) {
-                if (btns[0][j].getText().toString().equals("O"))
-                    return 1;//o win  - odd counting
-                return 2;
             }
 
 
         }
-        return -1;//no one win
+
     }
-    public void reset()
-    {
-        counter =0;
-        btnResetGame.setVisibility(View.INVISIBLE);
-        for(int i=0;i<3;i++)
-        {
-            for(int j=0;j<3;j++)
-                btns[i][j].setText("");
-        }
-        tvWIn.setVisibility(View.INVISIBLE);
-    }
+
+
+
+
+
+
+
+
+
+
 
 
     public void goMenu(View view) {
